0575139ef2185db9eb1d5af3f9addd61ae27d68b
Jordan Merli <jordanmerli90@gmail.com>
2025-10-15 14:00:30 +0200

    Message Box

---
+++ MSGBOX CREATER/Form1.Designer.vb
@@ -0,0 +1,223 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(172, 10)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(57, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Crea"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.Red
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.ForeColor = System.Drawing.Color.White
+        Me.TextBox1.Location = New System.Drawing.Point(20, 43)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(136, 65)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "Testo"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.Red
+        Me.TextBox2.ForeColor = System.Drawing.Color.White
+        Me.TextBox2.Location = New System.Drawing.Point(19, 17)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(87, 20)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Text = "Titolo"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(172, 39)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(99, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Exclamation Box"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(350, 39)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(70, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Info Box"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(327, 10)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(93, 23)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "Yes/No Box"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(277, 39)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(67, 23)
+        Me.Button5.TabIndex = 7
+        Me.Button5.Text = "Critical Box"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(235, 10)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(86, 23)
+        Me.Button6.TabIndex = 8
+        Me.Button6.Text = "Ok/Cancel"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(798, 269)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(12, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = """"
+        Me.Label2.Visible = False
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(173, 68)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ReadOnly = True
+        Me.TextBox3.Size = New System.Drawing.Size(247, 71)
+        Me.TextBox3.TabIndex = 10
+        Me.TextBox3.Text = "Codice Sorgente Visual BASIC"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(296, 170)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(124, 13)
+        Me.LinkLabel1.TabIndex = 13
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "joe_nanoteck@hotmail.it"
+        '
+        'LinkLabel2
+        '
+        Me.LinkLabel2.AutoSize = True
+        Me.LinkLabel2.Location = New System.Drawing.Point(170, 170)
+        Me.LinkLabel2.Name = "LinkLabel2"
+        Me.LinkLabel2.Size = New System.Drawing.Size(115, 13)
+        Me.LinkLabel2.TabIndex = 14
+        Me.LinkLabel2.TabStop = True
+        Me.LinkLabel2.Text = "nanoteck.altervista.org"
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(173, 144)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(247, 23)
+        Me.Button7.TabIndex = 15
+        Me.Button7.Text = "Copia!"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.jordan_merli.My.Resources.Resources.Cattura
+        Me.PictureBox1.Location = New System.Drawing.Point(10, 10)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(157, 157)
+        Me.PictureBox1.TabIndex = 12
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(425, 189)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.LinkLabel2)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Imparare a creare una TextBox"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents LinkLabel2 As System.Windows.Forms.LinkLabel
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+
+End Class
---
+++ MSGBOX CREATER/Form1.vb
@@ -0,0 +1,48 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        TextBox3.Text = "Codice Sorgente Visual BASIC"
+        MsgBox(TextBox1.Text, Title:=TextBox2.Text)
+        MsgBox("MsgBox(" + Label2.Text + TextBox1.Text + Label2.Text + ", " + "Title:=" + Label2.Text + TextBox2.Text + Label2.Text + ")", Title:="CODICE VB")
+        TextBox3.Text = ("MsgBox(" + Label2.Text + TextBox1.Text + Label2.Text + ", " + "Title:=" + Label2.Text + TextBox2.Text + Label2.Text + ")")
+    End Sub
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        TextBox3.Text = "Codice Sorgente Visual BASIC"
+        MsgBox(TextBox1.Text, MsgBoxStyle.Exclamation, Title:=TextBox2.Text)
+        MsgBox("MsgBox(" + Label2.Text + TextBox1.Text + Label2.Text + ", " + "MsgBoxStyle.Exclamation, " + "Title:=" + Label2.Text + TextBox2.Text + Label2.Text + ")", Title:="CODICE VB")
+        TextBox3.Text = ("MsgBox(" + Label2.Text + TextBox1.Text + Label2.Text + ", " + "MsgBoxStyle.Exclamation, " + "Title:=" + Label2.Text + TextBox2.Text + Label2.Text + ")")
+    End Sub
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        TextBox3.Text = "Codice Sorgente Visual BASIC"
+        MsgBox(TextBox1.Text, MsgBoxStyle.YesNo, Title:=TextBox2.Text)
+        MsgBox("MsgBox(" + Label2.Text + TextBox1.Text + Label2.Text + ", " + "MsgBoxStyle.YesNo, " + "Title:=" + Label2.Text + TextBox2.Text + Label2.Text + ")", Title:="CODICE VB")
+        TextBox3.Text = ("MsgBox(" + Label2.Text + TextBox1.Text + Label2.Text + ", " + "MsgBoxStyle.YesNo, " + "Title:=" + Label2.Text + TextBox2.Text + Label2.Text + ")")
+    End Sub
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        TextBox3.Text = "Codice Sorgente Visual BASIC"
+        MsgBox(TextBox1.Text, MsgBoxStyle.OkCancel, Title:=TextBox2.Text)
+        MsgBox("MsgBox(" + Label2.Text + TextBox1.Text + Label2.Text + ", " + "MsgBoxStyle.OkCancel, " + "Title:=" + Label2.Text + TextBox2.Text + Label2.Text + ")", Title:="CODICE VB")
+        TextBox3.Text = ("MsgBox(" + Label2.Text + TextBox1.Text + Label2.Text + ", " + "MsgBoxStyle.OkCancel, " + "Title:=" + Label2.Text + TextBox2.Text + Label2.Text + ")")
+    End Sub
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        TextBox3.Text = "Codice Sorgente Visual BASIC"
+        MsgBox(TextBox1.Text, MsgBoxStyle.Critical, Title:=TextBox2.Text)
+        MsgBox("MsgBox(" + Label2.Text + TextBox1.Text + Label2.Text + ", " + "MsgBoxStyle.Critical, " + "Title:=" + Label2.Text + TextBox2.Text + Label2.Text + ")", Title:="CODICE VB")
+        TextBox3.Text = ("MsgBox(" + Label2.Text + TextBox1.Text + Label2.Text + ", " + "MsgBoxStyle.Critical, " + "Title:=" + Label2.Text + TextBox2.Text + Label2.Text + ")")
+    End Sub
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        TextBox3.Text = "Codice Sorgente Visual BASIC"
+        MsgBox(TextBox1.Text, MsgBoxStyle.Information, Title:=TextBox2.Text)
+        MsgBox("MsgBox(" + Label2.Text + TextBox1.Text + Label2.Text + ", " + "MsgBoxStyle.Information, " + "Title:=" + Label2.Text + TextBox2.Text + Label2.Text + ")", Title:="CODICE VB")
+        TextBox3.Text = ("MsgBox(" + Label2.Text + TextBox1.Text + Label2.Text + ", " + "MsgBoxStyle.Information, " + "Title:=" + Label2.Text + TextBox2.Text + Label2.Text + ")")
+    End Sub
+    Private Sub LinkLabel1_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("mailto:joe_nanoteck@hotmail.it")
+    End Sub
+    Private Sub LinkLabel2_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        Process.Start("http://nanoteck.altervista.org")
+    End Sub
+    Private Sub Button7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button7.Click
+        Clipboard.SetText(TextBox3.Text)
+        MsgBox("Dati copiati correttamente negli appunti", MsgBoxStyle.Information, "Copia!")
+    End Sub
+End Class
